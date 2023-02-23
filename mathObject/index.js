c348051b8418f350b63dd6f85bcd737d058d5d18
yashwant.mangate-coditas <yashwant.mangate@coditas.com>
2023-02-23 19:45:28 +0530

    ADD done with the math objecct

---
+++ mathObject/index.js
@@ -0,0 +1,16 @@
+// document.write(Math.PI);
+// document.write(Math.E);
+
+const number = 49;
+const sqrtNum = Math.sqrt(number); //7
+// document.write("<h1>" + sqrtNum + "</h1>");
+
+// document.write(
+//   Math.max(1, 2, 3, 4, 5, 0, 6, 7, 8, 55, 6, 5, 13, 3, 4456, 1008, 4534, 23423)
+// ); //gives out the max among them
+
+// document.write(
+//   Math.min(1, 2, 3, 4, 5, 0, 6, 7, 8, 55, 6, 5, 13, 3, 4456, 1008, 4534, 23423)
+// ); //gives out the min among them
+
+document.write(Math.pow(10, 2));
