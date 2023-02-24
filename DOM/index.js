dea206b66f765efe3a7d9740e52e08864402edfb
yashwant.mangate-coditas <yashwant.mangate@coditas.com>
2023-02-24 10:40:41 +0530

    ADD done with the DOM

---
+++ DOM/index.js
@@ -0,0 +1,18 @@
+console.log("Welcome");
+
+const a = window;
+
+// alert("Hello!!");
+// console.log(a);
+
+// const num1 = parseInt(prompt("Enter the value of Num1:"));
+// const num2 = parseInt(prompt("Enter the value of Num2:"));
+
+// document.write(num1 + num2);
+
+// const confirmed = confirm("Are you sure");
+// console.log(confirmed);
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+console.log(window.document);
---
+++ DOM/index1.js
@@ -0,0 +1,5 @@
+function button() {
+  //   alert("Welcome here!");
+  let heading = document.getElementById("heading");
+  heading.innerHTML = "Button clicked, text changed";
+}
