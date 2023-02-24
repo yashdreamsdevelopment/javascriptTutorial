466af5d08117846cc864e800c35e1fa4d17bac1c
yashwant.mangate-coditas <yashwant.mangate@coditas.com>
2023-02-24 11:38:23 +0530

    ADD done til layout and DOM manipulation

---
+++ website1/index.js
@@ -0,0 +1,21 @@
+function OneS() {
+  //   const container = document.querySelector(".container");
+  //   const rightClasses = container.querySelector(".right");
+  //   const childrenOfContainer = container.children;
+  //   const childNodesOfContainer = container.childNodes;
+  //   console.log(childrenOfContainer);
+  //   console.log(childNodesOfContainer);
+  //   console.log(rightClasses);
+  //   console.log(container.firstChild);
+  //   console.log(container.firstElementChild);
+  //   console.log(container.lastChild);
+  //   console.log(container.lastElementChild);
+  //   console.log(container.parentNode); //body
+
+  const centerClassEle = document.querySelector(".center");
+
+  //   console.log(centerClassEle.nextElementSibling); // div.right
+  //   console.log(centerClassEle.nextSibling);
+  //   console.log(centerClassEle.previousElementSibling);
+  //   console.log(centerClassEle.previousSibling);
+}
