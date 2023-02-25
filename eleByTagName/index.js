9f4e250b812a7ca449eaef7ef3ea99e1b4880069
yashwant.mangate-coditas <yashwant.mangate@coditas.com>
2023-02-25 14:33:05 +0530

    ADD done with the getElementByTagName method

---
+++ eleByTagName/index.js
@@ -0,0 +1,8 @@
+function Fun2() {
+  const divTags = document.getElementsByTagName("div");
+
+  divTags[0].style.fontFamily = "Courier New";
+  divTags[1].style.color = "Blue";
+  divTags[2].style.fontStyle = "italic";
+  divTags[3].style.backgroundColor = "red";
+}
